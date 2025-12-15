211da0deee6481e8bf1c73518b7a1a4cdb6e0907
bala-amypo <bala@amypo.in>
2025-12-15 16:59:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/Appliance.java
+++ src/main/java/com/example/demo/entity/Appliance.java
@@ -1,5 +1,19 @@
 package com.example.demo.entity;
 
 public class Appliance {
+    private long id;
+    private String applianceName;
+    private Double wattage;
+    private Double usageHoursPerDay;
+    private User user;
+
+    public Appliance(){}
+    public Appliance(long id,String applianceName,Double wattage,Double usageHoursPerDay,User user){
+        this.id=id;
+        this.applianceName=applianceName;
+        this.wattage=wattage;
+        this.usageHoursPerDay=usageHoursPerDay;
+        this.user=user;
+    }
     
 }
