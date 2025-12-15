8aba7e5875796e5b7ebf6987b8edd16b2daffd14
bala-amypo <bala@amypo.in>
2025-12-15 17:06:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/Recommendation.java
+++ src/main/java/com/example/demo/entity/Recommendation.java
@@ -1,8 +1,43 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 public class Recommendation {
     private long id;
     private User user;
     private String message;
-    private LocalDateTime generatedO
+    private LocalDateTime generatedOn;
+
+    public Recommendation(){
+    }
+    public Recommendation(User user,String message,LocalDateTime generatedOn){
+        this.user=user;
+        this.message=message;
+        this.generatedOn=generatedOn;
+    }
+    public long getId() {
+        return id;
+    }
+    public User getUser() {
+        return user;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public LocalDateTime getGeneratedOn() {
+        return generatedOn;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public void setGeneratedOn(LocalDateTime generatedOn) {
+        this.generatedOn = generatedOn;
+    }
+    
 }
